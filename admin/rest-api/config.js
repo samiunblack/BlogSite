29c2272522ef1e204b9f042385c0adfde59201ec
samiun black <samiunblack@gmail.com>
2022-07-02 17:36:52 +0600

    add admin

--- admin/rest-api/config.js
+++ admin/rest-api/config.js
@@ -1,8 +1,8 @@
 const config = {
   devAdminURL: "http://localhost:3001",
-  prodAdminURL: "https://www.admin.example.com",
+  prodAdminURL: "https://www.admin.samiunblack.xyz",
   devFrontendWebsiteURL: "http://localhost:3000",
-  prodFrontendWebsiteURL: "https://www.example.com"
+  prodFrontendWebsiteURL: "https://www.samiunblack.xyz"
 }
 
 module.exports = config
--- admin/website/next.config.js
+++ admin/website/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     "DEV_ADMIN_API_URL": "http://localhost:5001",
-    "PRODUCTION_ADMIN_API_URL": "https://www.admin.api.samiunblack.xyz"
+    "PRODUCTION_ADMIN_API_URL": "https://www.admin-api.samiunblack.xyz"
   }
 }
